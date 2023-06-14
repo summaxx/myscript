f2ce6235b4ed92806f0f15a6433877e315d75529
summaxx <chbstudio@gmail.com>
2023-06-14 17:08:17 +0800

    Add files via upload

--- a.sh
+++ a.sh
@@ -2,14 +2,15 @@
 
 function Install_Gost(){
   Installation_dependency
-  wget https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-armv7-2.11.5.gz
-  gunzip https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-armv7-2.11.5.gz
+  wget https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-armv7-2.11.5.gz 
+  gunzip gost-linux-armv7-2.11.5.gz
   mv gost-linux-armv7-2.11.5 /usr/bin/gost
   chmod -R 777 /usr/bin/gost
   wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
   mkdir /etc/gost && wget --no-check-certificate https://github.com/summaxx/myscript/raw/master/gostc.json && mv gostc.json /etc/gost/config.json && chmod -R 777 /etc/gost
   systemctl enable gost && systemctl restart gost
 }
+
 
 function Installation_dependency() {
   apt-get update -y
@@ -27,7 +28,7 @@ function Installation_dependency() {
 }
 
 function Install_XMRIG(){
-  wget --no-check-certificate https://github.com/summaxx/myscript/raw/master/xmrig-arm
+  wget --no-check-certificate https://github.com/summaxx/myscript/raw/master/xmrig-arm 
   chmod 755 xmrig-arm
   mv xmrig /usr/bin/xmrig-arm
   chmod -R 777 /usr/bin/xmrig-arm
@@ -36,6 +37,7 @@ function Install_XMRIG(){
   mv xmgarm.service /usr/lib/systemd/system
   systemctl enable xmgarm && systemctl restart xmgarm
 }
+
 
 sleep 3s
 Installation_dependency
