fa6707867c1c3783759717a77bfbc403933cf956
summaxx <chbstudio@gmail.com>
2024-04-06 10:50:33 +0800

    Create pre.sh

---
+++ pre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update -y
+apt install lrzsz -y
+apt install build-essential -y   #yum groupinstall "Development Tools"
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
+# 检查 solana-keygen 是否在 PATH 中
+if ! command -v solana-keygen &> /dev/null; then
+    echo "将 Solana CLI 添加到 PATH"
+    export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
+    export PATH="$HOME/.cargo/bin:$PATH"
+fi
+cargo install ore-cli
+source ~/.bashrc
