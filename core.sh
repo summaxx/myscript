cd3208ed99a212c50db550a9e2999ef2894c7c4e
summaxx <chbstudio@gmail.com>
2024-04-09 15:19:18 +0800

    Update core.sh

--- core.sh
+++ core.sh
@@ -3,6 +3,9 @@
 sudo kill -9 $(sudo ps aux | grep -v "grep" | grep "./ore.sh" | awk '{print $2}')
 sudo sed -i 's/10000/1000/g' /root/ore.sh
 sudo killall ore
+sudo -i
+cargo install ore-cli
+exit
 sudo screen -S ore -X stuff './ore.sh^M'
 sleep 2
 ps a
