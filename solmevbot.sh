c52e9ce0449ccf318a56bb894850a4fce0b886e5
summaxx <chbstudio@gmail.com>
2025-04-17 13:18:30 +0800

    Update solmevbot.sh

--- solmevbot.sh
+++ solmevbot.sh
@@ -4,8 +4,6 @@ apt update
 apt install lrzsz net-tools unzip -y
 mkdir bot
 cd bot
-wget https://github.com/summaxx/myscript/raw/refs/heads/master/50-cloud-init.yaml
-mv 50-cloud-init.yaml /etc/netplan/
 wget https://sourceforge.net/projects/rust-mev-bot/files/rust-mev-bot-1.0.10.zip
 unzip rust-mev-bot-1.0.10.zip
 chmod +x upgrade.sh
