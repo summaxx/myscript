d12000b8691c7da7d4b8feeb281cb4b301e4bec2
summaxx <chbstudio@gmail.com>
2025-04-17 10:29:45 +0800

    mevbot-sol

---
+++ solmevbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+apt install lrzsz net-tools unzip -y
+wget https://sourceforge.net/projects/rust-mev-bot/files/rust-mev-bot-1.0.10.zip
+unzip rust-mev-bot-1.0.10.zip
+chmod +x upgrade.sh
+./upgrade.sh
+chmod +x run.sh
+cat>/etc/cloud/cloud.cfg.d/99-disable-network-config.cfg<<EOF
+network: {config: disabled}
+EOF
+echo "Do!"
