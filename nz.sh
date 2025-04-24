592c861035c7e322e93ad5752774501b8d0db4ce
summaxx <chbstudio@gmail.com>
2025-04-24 16:21:48 +0800

    Update nz.sh

--- nz.sh
+++ nz.sh
@@ -2,5 +2,5 @@
 cd
 sudo cp nezha-agent.service /etc/systemd/system/nezha-agent.service
 sudo chmod +x /etc/systemd/system/nezha-agent.service
-sudo systemctl start nezha-agent.service
+sudo systemctl restart nezha-agent.service
 echo "nezha ok"
